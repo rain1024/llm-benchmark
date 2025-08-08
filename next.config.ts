d5047f744eb5ca65f91eb4e5049a90c20a19f6c4
Vu Anh <anh.vu2@mservice.com.vn>
2025-08-08 13:43:37 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/llm-benchmark',
+  assetPrefix: '/llm-benchmark/',
   images: {
     unoptimized: true,
   },
